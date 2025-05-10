731090b32ec39d0132c779fc347c572437abcb07
maria.mikhailovskaya <mamikhajlovskaya@mai.education>
2025-05-10 17:44:44 +0300

    Initial commit: RTD demo

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'My Docs'
+author = 'Your Name'
+release = '0.1'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+My Docs
+=======
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   intro
